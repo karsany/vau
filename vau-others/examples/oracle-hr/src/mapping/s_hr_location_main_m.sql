b67eaca2c8be2ac8ee84d7660ee9fce9993e5b96
karsany <karsany@karsany.hu>
2018-02-11 23:59:51 +0100

    example mapping

--- vau-others/examples/oracle-hr/src/mapping/s_hr_location_main_m.sql
+++ vau-others/examples/oracle-hr/src/mapping/s_hr_location_main_m.sql
@@ -5,6 +5,12 @@
 -- load_method   cdc(ROW_EVENT, ROW_TS)
 -- sql --------------------------------
 
-Select 1 as LOCATION_BK,
-        null as postal_code ,        null as city ,        null as street_address ,        null as state_province ,        null as country_code   From dual
-
+Select t.location_id    As location_bk,
+       t.postal_code    As postal_code,
+       t.city           As city,
+       t.street_address As street_address,
+       t.state_province As state_province,
+       t.country_id     As country_code,
+       row_event,
+       row_ts
+  From hr.locations_h t
